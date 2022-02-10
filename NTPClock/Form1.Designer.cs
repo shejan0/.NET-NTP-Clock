b0b996626490b5f7047a0394430ba49abc404ac0
Shejan Shuza <shejan0@gmail.com>
2022-02-10 17:54:18 -0600

    Updated Layout

--- NTPClock/Form1.Designer.cs
+++ NTPClock/Form1.Designer.cs
@@ -32,45 +32,89 @@ namespace NTPClock
             this.SystemTime = new System.Windows.Forms.Label();
             this.NTPTime = new System.Windows.Forms.Label();
             this.DifferenceText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // SystemTime
             // 
             this.SystemTime.AutoSize = true;
-            this.SystemTime.Location = new System.Drawing.Point(49, 29);
+            this.SystemTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SystemTime.Location = new System.Drawing.Point(208, 103);
             this.SystemTime.Name = "SystemTime";
-            this.SystemTime.Size = new System.Drawing.Size(84, 17);
+            this.SystemTime.Size = new System.Drawing.Size(96, 17);
             this.SystemTime.TabIndex = 0;
             this.SystemTime.Text = "Getting time";
+            this.SystemTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // NTPTime
             // 
             this.NTPTime.AutoSize = true;
-            this.NTPTime.Location = new System.Drawing.Point(358, 217);
+            this.NTPTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NTPTime.Location = new System.Drawing.Point(208, 300);
             this.NTPTime.Name = "NTPTime";
-            this.NTPTime.Size = new System.Drawing.Size(84, 17);
+            this.NTPTime.Size = new System.Drawing.Size(96, 17);
             this.NTPTime.TabIndex = 1;
             this.NTPTime.Text = "Getting time";
+            this.NTPTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // DifferenceText
             // 
             this.DifferenceText.AutoSize = true;
-            this.DifferenceText.Location = new System.Drawing.Point(610, 29);
+            this.DifferenceText.Location = new System.Drawing.Point(195, 195);
             this.DifferenceText.Name = "DifferenceText";
             this.DifferenceText.Size = new System.Drawing.Size(121, 17);
             this.DifferenceText.TabIndex = 2;
             this.DifferenceText.Text = "Getting difference";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "System Clock Time";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(195, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NTP Clock Time";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(184, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Millisecond difference";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(494, 465);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.DifferenceText);
             this.Controls.Add(this.NTPTime);
             this.Controls.Add(this.SystemTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "NTP Clock Difference";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +125,9 @@ namespace NTPClock
         private System.Windows.Forms.Label SystemTime;
         private System.Windows.Forms.Label NTPTime;
         private System.Windows.Forms.Label DifferenceText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
