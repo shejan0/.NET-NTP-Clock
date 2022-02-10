35d31d422609bf2ea0f354429eb25add4642a7c2
Shejan Shuza <shejan0@gmail.com>
2022-02-10 17:45:26 -0600

    Add project files.

---
+++ NTPClock/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace NTPClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SystemTime = new System.Windows.Forms.Label();
+            this.NTPTime = new System.Windows.Forms.Label();
+            this.DifferenceText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SystemTime
+            // 
+            this.SystemTime.AutoSize = true;
+            this.SystemTime.Location = new System.Drawing.Point(49, 29);
+            this.SystemTime.Name = "SystemTime";
+            this.SystemTime.Size = new System.Drawing.Size(84, 17);
+            this.SystemTime.TabIndex = 0;
+            this.SystemTime.Text = "Getting time";
+            // 
+            // NTPTime
+            // 
+            this.NTPTime.AutoSize = true;
+            this.NTPTime.Location = new System.Drawing.Point(358, 217);
+            this.NTPTime.Name = "NTPTime";
+            this.NTPTime.Size = new System.Drawing.Size(84, 17);
+            this.NTPTime.TabIndex = 1;
+            this.NTPTime.Text = "Getting time";
+            // 
+            // DifferenceText
+            // 
+            this.DifferenceText.AutoSize = true;
+            this.DifferenceText.Location = new System.Drawing.Point(610, 29);
+            this.DifferenceText.Name = "DifferenceText";
+            this.DifferenceText.Size = new System.Drawing.Size(121, 17);
+            this.DifferenceText.TabIndex = 2;
+            this.DifferenceText.Text = "Getting difference";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DifferenceText);
+            this.Controls.Add(this.NTPTime);
+            this.Controls.Add(this.SystemTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label SystemTime;
+        private System.Windows.Forms.Label NTPTime;
+        private System.Windows.Forms.Label DifferenceText;
+    }
+}
+
---
+++ NTPClock/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GuerrillaNtp;
+namespace NTPClock
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        NtpClient ntpClient;
+        TimeSpan difference;
+        int times=0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 1;
+            timer.Enabled = true;
+            timer.Tick += Timer_Tick;
+            try
+            {
+                ntpClient = new NtpClient(Dns.GetHostAddresses("pool.ntp.org")[0]);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+
+        }
+        void UpdateTimes()
+        {
+
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            times++;
+            SystemTime.Text = DateTime.UtcNow.ToString();
+            if (times % 2000==0)
+            {
+                try
+                {
+                    difference = ntpClient.GetCorrectionOffset();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            DifferenceText.Text = difference.TotalMilliseconds.ToString();
+            NTPTime.Text = (DateTime.UtcNow + difference).ToString();
+            
+        }
+    }
+}
